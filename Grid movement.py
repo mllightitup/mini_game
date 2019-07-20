b5506b9905e6d21d63261647133e06ce54dbef9d
mllightitup <43480503+mllightitup@users.noreply.github.com>
2019-07-20 17:22:08 +0300

    Add files via upload

---
+++ Grid movement.py
@@ -0,0 +1,35 @@
+def ret_pos(grid, value): ## возвращает позицию игрока
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == value:
+                output=[i,j]
+    return(output)
+
+GRID_SIZE = 10
+
+grid = [['#' for i in range(GRID_SIZE)] for i in range(GRID_SIZE)]
+x = 1
+y = 1
+grid[x][y] = 'P'
+
+while True:
+    print('Текущая доска:')
+    for j in range(GRID_SIZE):
+        s = ''
+        for k in range(GRID_SIZE):
+            s += f'{grid[j][k]} '
+        print(s)
+
+    print('')
+    print("Куда идем?")
+    command = input()
+    grid[x][y] = '#'
+    if command == 'верх' and x > 0:
+        x -= 1
+    elif command == 'низ' and x < GRID_SIZE - 1:
+        x += 1
+    elif command == 'лево' and y > 0:
+        y -= 1
+    elif command == 'право' and y < GRID_SIZE - 1:
+        y += 1
+    grid[x][y] = 'P'
